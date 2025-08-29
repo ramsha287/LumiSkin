9a5c9e4d4b49f64899c2a0b9cff44fed6f284fcc
ramsha287 <arshadramsha04@gmail.com>
2025-08-29 20:13:49 +0530

    url changed

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(helmet());
 app.use(cors({
-  origin: "https://lumiskin-skincare.netlify.app/", 
+  origin: "https://lumiskin-skincare.netlify.app", 
   credentials: true
 }));
 app.use(morgan("combined"));
